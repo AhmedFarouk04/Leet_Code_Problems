1d445bba3bf58b73830ccf40c905730ba5fafc85
a7meddidy <af7974943@gmail.com>
2026-01-14 14:56:36 +0200

    Add Folder week02 and solve first problem Valid Anagram

---
+++ Week02_Hashing/Valid Anagram/README.md
@@ -0,0 +1,14 @@
+﻿# Valid Anagram
+
+**Pattern:** Frequency Map (Hashing)
+
+## Idea
+- Count characters in first string
+- Subtract counts using second string
+- If all counts are zero → anagram
+
+## Time Complexity
+- O(n)
+
+## Space Complexity
+- O(n)
---
+++ Week02_Hashing/Valid Anagram/ValidAnagram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week02_Hashing.Valid_Anagram
+{
+    public  class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (!freq.ContainsKey(c))
+                    freq[c] = 0;
+
+                freq[c]++;
+            }
+
+            foreach (char c in t)
+            {
+                if (!freq.ContainsKey(c))
+                    return false;
+
+                freq[c]--;
+
+                if (freq[c] < 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
