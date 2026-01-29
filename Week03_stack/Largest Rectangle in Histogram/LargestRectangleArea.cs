9bb70132d51d0492c326792a5d8837c6b80d418c
a7meddidy <af7974943@gmail.com>
2026-01-29 10:10:20 +0200

    Add  Largest Rectangle in Histogram   Problem

---
+++ Week03_stack/Largest Rectangle in Histogram/LargestRectangleArea.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week03_stack.Largest_Rectangle_in_Histogram
+{
+    public  class LargestRectangleArea
+    {
+        public int Larges_tRectangle_Area(int[] heights)
+        {
+            Stack<int> stack = new Stack<int>();
+            int maxArea = 0;
+            int n = heights.Length;
+
+            for (int i = 0; i <= n; i++)
+            {
+                int currHeight = (i == n) ? 0 : heights[i];
+
+                while (stack.Count > 0 && currHeight < heights[stack.Peek()])
+                {
+                    int h = heights[stack.Pop()];
+                    int right = i;
+                    int left = stack.Count == 0 ? -1 : stack.Peek();
+                    int width = right - left - 1;
+                    maxArea = Math.Max(maxArea, h * width);
+                }
+
+                stack.Push(i);
+            }
+
+            return maxArea;
+        }
+
+    }
+}
---
+++ Week03_stack/Largest Rectangle in Histogram/README.md
@@ -0,0 +1,32 @@
+﻿Largest Rectangle in Histogram
+
+Goal:
+Find the largest rectangle area inside a histogram.
+
+Idea:
+
+Use a monotonic increasing stack
+
+Stack stores indices, not heights
+
+When a smaller height appears → we finalize areas
+
+Key Insight:
+When a bar is popped:
+
+It is the smallest height in its rectangle
+
+Left boundary = new stack top
+
+Right boundary = current index
+
+Why Stack?
+Because each bar is pushed and popped once only
+
+Time Complexity:
+
+O(n)
+
+Space Complexity:
+
+O(n)
